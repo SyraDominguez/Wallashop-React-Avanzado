4321f0f4e1c0d03d8fe57d8cc626487219a1390f
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-01 10:30:42 +0200

    first version main page

--- wallashop/src/pages/ads/adsPage.jsx
+++ wallashop/src/pages/ads/adsPage.jsx
@@ -1,62 +1,75 @@
-const initialProducts = [
-  {
-    "name": "Airpods",
-    "sale": false,
-    "price": 80,
-    "photo": "airpods.jpg",
-    "tags": [
-      "work",
-      "lifestyle"
-    ]
-  },
-  {
-    "name": "Alexa",
-    "sale": true,
-    "price": 125.75,
-    "photo": "alexa.jpg",
-    "tags": [
-      "lifestyle",
-      "mobile"
-    ]
-  },
-  {
-    "name": "Camara Canon",
-    "sale": true,
-    "price": 535,
-    "photo": "canon.jpg",
-    "tags": [
-      "work",
-      "lifestyle"
-    ]
-  },
-  {
-    "name": "Toyota CH-R",
-    "sale": false,
-    "price": 35700,
-    "photo": "chr.jpg",
-    "tags": [
-      "work",
-      "motor"
-    ]
-  },
-  {
-    "name": "MacBook Pro",
-    "sale": false,
-    "price": 850,
-    "photo": "macbook_pro.jpg",
-    "tags": [
-      "work",
-      "lifestyle"
-    ]
-  }
-]
+import { useEffect, useState } from 'react';
+
+import styles from './adsPage.module.css';
+import { getLatestAds } from './service';
+
+// const Products = [
+//   {
+//     "name": "Airpods",
+//     "sale": false,
+//     "price": 80,
+//     "photo": "airpods.jpg",
+//     "tags": [
+//       "work",
+//       "lifestyle"
+//     ]
+//   },
+//   {
+//     "name": "Alexa",
+//     "sale": true,
+//     "price": 125.75,
+//     "photo": "alexa.jpg",
+//     "tags": [
+//       "lifestyle",
+//       "mobile"
+//     ]
+//   },
+//   {
+//     "name": "Camara Canon",
+//     "sale": true,
+//     "price": 535,
+//     "photo": "canon.jpg",
+//     "tags": [
+//       "work",
+//       "lifestyle"
+//     ]
+//   },
+//   {
+//     "name": "Toyota CH-R",
+//     "sale": false,
+//     "price": 35700,
+//     "photo": "chr.jpg",
+//     "tags": [
+//       "work",
+//       "motor"
+//     ]
+//   },
+//   {
+//     "name": "MacBook Pro",
+//     "sale": false,
+//     "price": 850,
+//     "photo": "macbook_pro.jpg",
+//     "tags": [
+//       "work",
+//       "lifestyle"
+//     ]
+//   }
+// ]
+
 
 function AdsPage() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => { 
+    getLatestAds().then(ads => setProducts(ads));
+  }, [])
+
   return (
-    <div>
+    <div className={styles.adsPage}>
       <h1>Ads Page</h1>
       <ul>
-        {initialProducts.map((product) => (
+        {products.map((product) => (
           <li key={product.id}>
             <h2>{product.name}</h2>
             <p>{product.description}</p>
