ea2bd468b1ed01cd6c16721df5b77ed93d7dd484
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-01 13:07:01 +0200

    controlled login inputs

--- wallashop/src/api/client.js
+++ wallashop/src/api/client.js
@@ -6,4 +6,5 @@ export const client = axios.create({
 
 client.interceptors.response.use(response => response.data);
 
-export const setAuthorizationHeader = token => client.defaults.headers.common['Authorization'] = `Bearer ${token}`; 
+export const setAuthorizationHeader = token =>
+  (client.defaults.headers.common['Authorization'] = `Bearer ${token}`);
--- wallashop/src/pages/auth/loginPage.jsx
+++ wallashop/src/pages/auth/loginPage.jsx
@@ -1,34 +1,53 @@
+import { useState } from "react";
 import Button from "../../components/button";
 import { login } from "./service";
 
-export default function LoginPage (onLogin) {
-  const handleSubmit = async event => {
+export default function LoginPage(onLogin) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const response = await login({
-        email: event.target.email.value,
-        password: event.target.password.value
+        email,
+        password,
       });
       onLogin(true);
-      
     } catch (error) {
       console.error(error);
     }
-
-
-
-
-  }
+  };
 
   return (
-  <div>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit}>
-      <input type = "email" name = "email" placeholder = "Email" required/>
-      <input type = "password" name = "password" placeholder = "Password" required/>
-      <Button type="submit" $variant= "primary">Login</Button>
-    </form>
-  </div>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          required
+        />
+        <Button type="submit" $variant="primary">
+          Login
+        </Button>
+      </form>
+    </div>
   );
 }
