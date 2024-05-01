2cf5827ded4e7167d104e1ba2c0a20a8a791985b
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-01 10:30:11 +0200

    button component created with styles

--- wallashop/src/App.jsx
+++ wallashop/src/App.jsx
@@ -1,7 +1,31 @@
-import AdsPage from "./pages/ads/adsPage";
+import AdsPage from "./pages/ads/adsPage.jsx";
+import Button from "./components/button.jsx";
 
 function App() {
-  return <AdsPage />
+  return (
+  <div>
+    <Button
+      onClick={() => { 
+        console.log('Click')
+        }}
+        $variant = "primary"
+      >
+          Login
+    </Button>
+
+    <Button 
+      onClick={() => { 
+        console.log('Click')
+        }}
+        $variant = "primary"
+        >
+          Register
+    </Button>
+
+
+    <AdsPage />
+  </div>
+  );
 }
 
 export default App
