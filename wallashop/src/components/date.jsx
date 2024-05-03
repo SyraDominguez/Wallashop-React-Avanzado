5241fdd9f62acce1f12c10184c5c0e2fac9a1499
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-03 12:40:41 +0200

    date, time and geolocation component

---
+++ wallashop/src/components/date.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function DateTime() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    }
+
+    // con api para convertir latitud y longitud en ciudad
+    /*
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+
+        axios
+          .get(
+            `https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=TU_CLAVE_DE_API`
+          )
+          .then((response) => {
+            setCity(response.data.results[0].components.city);
+          });
+      });
+    }
+    */
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <span>
+      {currentDate.toLocaleString()} - Ubicación:{" "}
+      {location
+        ? `Latitud: ${location.latitude}, Longitud: ${location.longitude}`
+        : "Ubicación no disponible"}
+    </span>
+  );
+}
+
+export default DateTime;
