f16b8bfb3522a322f65101549c2098220d32d669
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-03 10:11:31 +0200

    data persistence functionality

--- wallashop/src/App.jsx
+++ wallashop/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AdsPage from "./pages/ads/adsPage";
 import NewAdPage from "./pages/ads/newAdPage";
 import LoginPage from "./pages/auth/loginPage";
+import storage from "./storage";
 
 function App({ isDefaultLogged }) {
   const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    const token = storage.get("auth");
+    if (token) {
+      setIsLogged(true);
+    }
+  }, []);
 
   const handleLogin = () => {
     setIsLogged(true);
