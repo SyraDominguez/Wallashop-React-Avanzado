52c5e9530ea1c97b38db7e1271e7bf278597f3a4
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-03 20:42:04 +0200

    message in case of empty ads

--- wallashop/src/pages/ads/adsPage.jsx
+++ wallashop/src/pages/ads/adsPage.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
-
 import styles from "./adsPage.module.css";
 import adsData from "./adsTest.json";
 import { getLatestAds } from "./service";
 import Button from "../../components/button";
 import DateTime from "../../components/date";
-
 import Layout from "../../components/layout/layout";
+
+const EmptyList = () => (
+  <div className={styles.emptyList}>
+    <p>
+      ¡Haz historia y sé el primero en destacar con tu anuncio en nuestra
+      plataforma!
+    </p>
+    <Button $varian="primary">Crear un anuncio</Button>
+  </div>
+);
 
 function AdsPage() {
   const [products, setProducts] = useState(adsData);
@@ -21,22 +29,26 @@ function AdsPage() {
     <Layout title="Anuncios destacados de hoy en tu ciudad">
       <DateTime />
       <div className={styles.adsPage}>
-        <ul className={`${styles.adsGrid} ${styles.adsContainer}`}>
-          {products.map((product) => (
-            <li key={product.name} className={styles.adCard}>
-              <h5>{product.name}</h5>
-              <p>{product.description}</p>
-              <p>{product.price} €</p>
-              <img src={product.photo} alt={product.name} />
-              <Button>{product.sale ? "Sale" : "Buy"}</Button>
-              <ul>
-                {product.tags.map((tag, index) => (
-                  <li key={index}>{tag}</li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
+        {products.length ? (
+          <ul className={`${styles.adsGrid} ${styles.adsContainer}`}>
+            {products.map((product) => (
+              <li key={product.name} className={styles.adCard}>
+                <h5>{product.name}</h5>
+                <p>{product.description}</p>
+                <p>{product.price} €</p>
+                <img src={product.photo} alt={product.name} />
+                <Button>{product.sale ? "Sale" : "Buy"}</Button>
+                <ul>
+                  {product.tags.map((tag, index) => (
+                    <li key={index}>{tag}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <EmptyList />
+        )}
       </div>
     </Layout>
   );
