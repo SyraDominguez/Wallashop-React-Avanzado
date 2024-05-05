e432ea7e0c38c93385f450d772777bf4a4acebd4
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-05 14:12:33 +0200

    modified the button rendering according to the page

--- wallashop/src/pages/auth/AuthButton.jsx
+++ wallashop/src/pages/auth/AuthButton.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Button from "../../components/button";
 import { useAuth } from "./context";
 import { logout } from "./service";
@@ -7,11 +7,16 @@ import PropTypes from "prop-types";
 
 function AuthButton({ className }) {
   const { isLogged, onLogout } = useAuth();
+  const location = useLocation();
 
   const handleLogoutClick = async () => {
     await logout();
     onLogout();
   };
+
+  if (location.pathname === "/login") {
+    return null; // No renderizar ningún botón si estamos en la página de login
+  }
 
   return isLogged ? (
     <Button onClick={handleLogoutClick} className={className}>
@@ -23,5 +28,9 @@ function AuthButton({ className }) {
     </Button>
   );
 }
+
+AuthButton.propTypes = {
+  className: PropTypes.string,
+};
 
 export default AuthButton;
