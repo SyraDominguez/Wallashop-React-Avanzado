309b5dd34e799b139c9c21be7db0876bfed41141
Syra Dominguez <syradominguez.dev@gmail.com>
2024-06-21 12:31:11 +0200

    feat: Add unit test for setAds action in adActionsSync.test.js

---
+++ wallashop/__test__/adActionsSync.test.js
@@ -0,0 +1,30 @@
+
+jest.mock('../src/api/client', () => {
+  return {
+    client: {
+      defaults: {
+        headers: {
+          common: {}
+        }
+      },
+      get: jest.fn(),
+      post: jest.fn(),
+      delete: jest.fn()
+    },
+    setAuthorizationHeader: jest.fn(),
+    removeAuthorizationHeader: jest.fn()
+  };
+});
+
+import { setAds } from '../src/store/actions/adActions';
+
+describe('adActions', () => {
+  it('should create an action to set ads', () => {
+    const ads = [{ id: 1, name: 'Test Ad' }];
+    const expectedAction = {
+      type: 'ads/setAds',
+      payload: { ads },
+    };
+    expect(setAds({ ads })).toEqual(expectedAction);
+  });
+});
